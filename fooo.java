d5048049a0cd9451631a1650ebe853e919e1cdad
prabavathiieswarn <jayalashmihp@gmail.com>
2026-03-06 14:29:11 +0530

    Add Main class with food preference logic

---
+++ fooo.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) { 
+	    int like =1;
+	int food=2;
+	if ( food==like )
+	{
+		System.out.println(" i like food");}
+		else if (like >food){
+	System.out.println("more food");}
+	else {
+	System.out.println("femous");
+	
+	
+	
+	
+	}
+	
+	    
+	
+	}
+}
